9fe5c977251ab740b722aa7dc02a4c00f31f13e4
yuno-kaihara <yuno.kaihara@gmail.com>
2026-03-05 20:34:27 +0900

    スクラッチ操作の実装

--- src/components/actionOverlay.tsx
+++ src/components/actionOverlay.tsx
@@ -6,6 +6,7 @@ import IdeaCheckList from "@/components/ideaCheckList";
 import LineDrawing from "@/components/lineDrawing";
 import FillAndErase from "@/components/fillAndErase";
 import DragLight from "@/components/dragLight";
+import Scratch from "./scratch";
 
 export default function ActionOverlay() {
   const { currentScene } = useScene();
@@ -23,6 +24,20 @@ export default function ActionOverlay() {
       return <FillAndErase mode={"erase"} />;
     case "drag_light":
       return <DragLight />;
+    case "scratch1":
+      return (
+        <Scratch
+          maskImage={"/images/sample1.png"}
+          underImage={"/images/sample2.png"}
+        />
+      );
+    case "scratch2":
+      return (
+        <Scratch
+          maskImage={"/images/sample3.png"}
+          underImage={"/images/sample4.png"}
+        />
+      );
     default:
       return null;
   }
